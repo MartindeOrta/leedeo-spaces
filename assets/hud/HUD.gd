90a1509012daa41311237cb7eb7542ea8fbbbc29
MartindeOrta <martindeorta@gmail.com>
2023-09-19 14:40:02 -0300

    listo

---
+++ assets/hud/HUD.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+signal game_over
+
+func _ready():
+	GLOBAL.score = 0
+	
+func _physics_process(delta):
+	$ScoreConteiner/HBoxContainer/Score.text =str(GLOBAL.score)
+
+
+func game_over():
+	emit_signal("game_over")
+	$GameOverContainer.visible = true
+	$BgMusic.play()
+
+func _on_Restart_pressed():
+	get_tree().call_deferred("reload_current_scene") # Replace with function body.
+
+
+func _on_Menu_pressed():
+	get_tree().call_deferred("change_scene", "res://assets/menu/Menu.tscn")
+	
+	
+func _on_Player_tree_exiting():
+	game_over() # Replace with function body.
--- assets/level/Level.gd
+++ assets/level/Level.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+export (PackedScene) var Enemy
+
 func _ready():
 	$bgMusic.play()
+	$EnemyTimer.start()
+	randomize()
 	
 func _physics_process(delta):
 	get_node("Background").scroll_base_offset += Vector2(0,1) * 8 * delta
 	get_node("Cloud01").scroll_base_offset += Vector2(0, 1) * 24 * delta
 	get_node("Cloud02").scroll_base_offset += Vector2(0, 1) * 34 * delta
+
+
+func _on_HUD_game_over():
+	$bgMusic.stop() # Replace with function body.
+
+
+func _on_EnemyTimer_timeout():
+	get_node("EnemyPath/EnemySpawn").set_offset(randi())
+	var enemy = Enemy.instance()
+	add_child(enemy) # Replace with function body.
+	enemy.position = get_node("EnemyPath/EnemySpawn").position
+	$EnemyTimer.wait_time = GLOBAL.random(1,3)
+	$EnemyTimer.start()
+	
